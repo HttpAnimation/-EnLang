b377de73b5201cfd8daebdf6ba02a4bc70c84704
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-15 10:38:58 -0500

    downloads backend

--- InstallForGit.bash
+++ InstallForGit.bash
@@ -7,4 +7,6 @@ git clone -b blank https://github.com/HttpAnimation/-EnLang.git
 mv -- -EnLang blank
 git clone -b gh-pages https://github.com/HttpAnimation/-EnLang.git
 mv -- -EnLang gh-pages
+git clone -b backend https://github.com/HttpAnimation/-EnLang.git
+mv -- -EnLang backend
 echo "Done"
