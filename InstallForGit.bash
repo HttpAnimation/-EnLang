15d9e92307ffb28629dcbcf901287ae076c39833
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-02-14 22:01:50 -0700

    Update InstallForGit.bash

--- InstallForGit.bash
+++ InstallForGit.bash
@@ -1,5 +1,6 @@
 cd ~/Desktop
 mkdir EnLang
+cd EnLand
 git clone -b main https://github.com/HttpAnimation/-EnLang.git
 mv -EnLang main
 git clone -b blank https://github.com/HttpAnimation/-EnLang.git
