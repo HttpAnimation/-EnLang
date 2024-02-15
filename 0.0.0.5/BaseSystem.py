e60469ae2a2a9a60ec6d1d8833d5c2e3b0533164
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-15 11:21:21 -0500

    adds 0.0.0.5

---
+++ 0.0.0.5/BaseSystem.py
@@ -0,0 +1,59 @@
+import subprocess
+import webbrowser
+import sys
+
+class EKGInterpreter:
+    def __init__(self):
+        self.imported_commands = set()
+        self.variables = {}
+        self.commands = {
+            "say": self.say,
+            "run.sys.command": self.run_sys_command,
+            "open.browser": self.open_browser,
+            "run.py.script": self.run_python_script,
+            "ask.question": self.ask_question
+        }
+
+    def interpret(self, filename):
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                tokens = line.strip().split(' ')
+                if tokens[0] == "get":
+                    if len(tokens) >= 2:
+                        self.imported_commands.add(tokens[1])
+                    else:
+                        print("Invalid import statement")
+                else:
+                    command = tokens.pop(0)
+                    if command in self.imported_commands:
+                        if command in self.commands:
+                            self.commands[command](' '.join(tokens))
+                        else:
+                            print(f"Unknown command: {command}")
+                    else:
+                        print(f"Command not imported: {command}")
+
+    def say(self, message):
+        print(message)
+
+    def run_sys_command(self, command):
+        subprocess.run(command, shell=True)
+
+    def open_browser(self, url):
+        webbrowser.open(url)
+
+    def run_python_script(self, script_name):
+        try:
+            exec(open(script_name).read(), globals())
+        except Exception as e:
+            print(f"Error running Python script: {e}")
+
+    def ask_question(self, question):
+        response = input(question + " ")
+        self.variables['response'] = response
+
+if __name__ == "__main__":
+    interpreter = EKGInterpreter()
+    filename = input("Enter the filename to interpret: ")
+    interpreter.interpret(filename)
