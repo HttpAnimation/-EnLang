f3f62a7bb42486c54435d6b09edd0b7affa91271
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-15 15:32:37 -0500

    adds maker

--- ProjectMaker/main.bash
+++ ProjectMaker/main.bash
@@ -1,4 +1,9 @@
 mkdir Project
 cd Project
 mkdir public
-wget 
+wget https://raw.githubusercontent.com/HttpAnimation/-EnLang/main/ProjectMaker/About.json
+cd public
+wget https://raw.githubusercontent.com/HttpAnimation/-EnLang/main/ProjectMaker/main.ekg
+cd ../
+cd ../
+echo "Done you can rename the project by renameing the folder project and changing the name in About.json"
